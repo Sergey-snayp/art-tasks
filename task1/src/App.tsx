953cc97f38eda1962fb14ad5b109c32a41c23317
Sergey-snayp <59452160+Sergey-snayp@users.noreply.github.com>
2023-08-15 17:03:00 +0300

    implemented tasks

---
+++ task1/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+// @ts-ignore
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <ul>
+            <li><a className="cool-link" href="#">FIRST</a></li>
+            <li><a className="cool-link" href="#">SECOND</a></li>
+            <li><a className="cool-link" href="#">SOME THIRD TEXT</a></li>
+        </ul>
+    </div>
+  );
+}
+
+export default App;
